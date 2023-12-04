3b6ead50b14ea543b0463d6e82237180d2134f51
Евгений Тупенко <atupenko@gmail.com>
2023-12-04 17:39:25 +0300

    add firbase config

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,17 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCkGMT2hMpfPUVRrpBHjSmw3SWT13ZHFrM",
+  authDomain: "pixema-2a2c8.firebaseapp.com",
+  projectId: "pixema-2a2c8",
+  storageBucket: "pixema-2a2c8.appspot.com",
+  messagingSenderId: "19736187209",
+  appId: "1:19736187209:web:931e2842923fd4afd88a98",
 };
 
 const app = initializeApp(firebaseConfig);
 
-// export const auth = getAuth(app);
-// export const user = auth.currentUser;
-// console.log(user);
 export default app;
